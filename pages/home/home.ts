b65be9e61362a62cfcd5405b138fd60c777155a7
J0-qui <joaquin.silvoza150194@my.lsgh.edu.ph>
2021-04-22 10:13:10 +0800

    2 files modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -6,7 +6,14 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  height: number;
+  weight: number;
+  bmi: number;
+  bmiMessage: string;
+calculateBMI() {
+  this.bmi = this.weight / (this.height * this.height);
+  this.bmi = parseFloat(this.bmi.toFixed(2));
+  
   constructor(public navCtrl: NavController) {
 
   }
